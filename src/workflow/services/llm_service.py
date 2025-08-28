befdf1cf2286b4b2ddc39cfaf1d3d28067195a63
BrenDev0 <lahey1991@gmail.com>
2025-08-28 10:05:22 -0600

    services for llm

---
+++ src/workflow/services/llm_service.py
@@ -0,0 +1,14 @@
+from langchain_openai import ChatOpenAI
+
+class LlmService:
+    def get_llm(
+        temperature: float,
+        max_tokens: int = None
+    ):
+        llm = ChatOpenAI(
+            model="gpt-4o",
+            temperature=temperature,
+            max_completion_tokens=max_tokens
+        )
+
+        return llm
