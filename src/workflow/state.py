d6ebe6a08d5c306f643e7e6fe0253e984c7ffd58
BrenDev0 <lahey1991@gmail.com>
2025-09-01 17:54:40 -0600

    state change to include supervisor resonse model

--- src/workflow/state.py
+++ src/workflow/state.py
@@ -1,7 +1,9 @@
 from typing_extensions import TypedDict
 from typing import List, Dict
+from uuid import UUID
+from src.workflow.agents.supervisor.supervisor_models import SupervisorOutput
 
 class State(TypedDict):
     input: str
-    available_agents: List[str]       
-    selected_agents: List[str]           
+    available_agents: List[UUID]       
+    selected_agents: SupervisorOutput          
